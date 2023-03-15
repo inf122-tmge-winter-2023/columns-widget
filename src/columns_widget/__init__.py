014df584dc01770294446bd2d3168a2d777a4856
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-14 21:37:41 -0700

    Add logging facilities

--- src/columns_widget/__init__.py
+++ src/columns_widget/__init__.py
@@ -4,3 +4,17 @@
     :module_author: Nathan Mendoza (nathancm@uci.edu)
 """
 
+import logging
+
+LOGGER = logging.getLogger(__name__)
+LOG_HANDLER = logging.StreamHandler()
+LOG_FORMAT = logging.Formatter('[%(asctime)s|%(name)s|%(levelname)s] - %(message)s')
+
+LOGGER.setLevel(logging.DEBUG)
+LOG_HANDLER.setLevel(logging.DEBUG)
+
+LOG_HANDLER.setFormatter(LOG_FORMAT)
+LOGGER.addHandler(LOG_HANDLER)
+
+__version__ = (0, 0, 1)
+
