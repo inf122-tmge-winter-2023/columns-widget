7eae5801d39c37db07d5daff17ae77597329b76e
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-21 23:06:44 -0700

    Adjust to new game engine interface

--- src/columns_widget/cli.py
+++ src/columns_widget/cli.py
@@ -8,10 +8,9 @@ import click
 
 from . import ColumnsGameState, ColumnsView, ColumnsGameLoop, ColumnsBoard, ColumnsScoring
 
-from tilematch_tools import GameEngine, BoardFactory
-from tilematch_tools.core.game_loop import FPSDelay
+from tilematch_tools import GameEngine, BoardFactory, Game
 
-def columns_init() -> ColumnsGameLoop:
+def columns_init() -> Game:
     """
         Initialize objects needed to start a game of columns
         :returns: game objects
@@ -20,15 +19,12 @@ def columns_init() -> ColumnsGameLoop:
     board = BoardFactory.create_board(ColumnsBoard, ColumnsBoard.COLUMNS_BOARD_WIDTH, ColumnsBoard.COLUMNS_BOARD_HEIGHT)
     score = ColumnsScoring()
     state = ColumnsGameState(board, score)
-    view = ColumnsView(state)
-    loop = ColumnsGameLoop(state, view, 750_000_000)
-    return loop
+    return Game(state, ColumnsGameLoop, ColumnsView)
 
 @click.command()
 def columns():
     """Entry point to columns"""
     ge = GameEngine([columns_init()])
-    ge._game_loops[0].view.add_event_listener("KeyRelease")
     ge.run()
 
     
