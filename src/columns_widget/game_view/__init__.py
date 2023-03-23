f52f4b7a9ccb202416d8583093a0d18fd57d94bc
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-22 20:27:17 -0700

    Add title to game

--- src/columns_widget/game_view/__init__.py
+++ src/columns_widget/game_view/__init__.py
@@ -10,7 +10,7 @@ from tilematch_tools import GameView, GameState, GameEvent
 class ColumnsView(GameView):
     """Class representing the columns view"""
     def __init__(self, parent, game_state: GameState):
-        super().__init__(parent, game_state)
+        super().__init__(parent, game_state, 'Columns')
 
 class ShiftFallerLeft(GameEvent):
     def __call__(self, event):
